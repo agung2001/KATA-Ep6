c6b555f099bf9b2a1209bc8ce467f79f4a349288
Agung Sundoro <muhagung2001@gmail.com>
2024-10-04 12:45:40 +0700

    fix: remove book type - make it single setup

--- bin/export-epub.sh
+++ bin/export-epub.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-# Load .env file
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-else
-    echo "❌ .env file not found!"
-    exit 1
-fi
-
-# Use the variables in conditions
-if [ "$BUNDLE" = "ALL" ]; then
-    asciidoctor-epub3 -vwt -a stylesheet=style.css -o output/book.epub free.adoc
-    asciidoctor-epub3 -vwt -a stylesheet=style.css -o output/book.epub book.adoc
-elif [ "$BUNDLE" = "FREE" ]; then
-    asciidoctor-epub3 -vwt -a stylesheet=style.css -o output/book.epub free.adoc
-else
-    asciidoctor-epub3 -vwt -a stylesheet=style.css -o output/book.epub book.adoc
-fi
+asciidoctor-epub3 -vwt -a stylesheet=style.css -o output/book.epub book.adoc
--- bin/export-pdf.sh
+++ bin/export-pdf.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
 
-# Load .env file
-if [ -f .env ]; then
-    export $(grep -v '^#' .env | xargs)
-else
-    echo "❌ .env file not found!"
-    exit 1
-fi
-
-# Use the variables in conditions
-if [ "$BUNDLE" = "ALL" ]; then
-    asciidoctor-pdf -vwt -a pdf-theme=theme.yml -o output/free.pdf free.adoc
-    asciidoctor-pdf -vwt -a pdf-theme=theme.yml -o output/book.pdf book.adoc
-elif [ "$BUNDLE" = "FREE" ]; then
-    asciidoctor-pdf -vwt -a pdf-theme=theme.yml -o output/free.pdf free.adoc
-else
-    asciidoctor-pdf -vwt -a pdf-theme=theme.yml -o output/book.pdf book.adoc
-fi
+asciidoctor-pdf -vwt -a pdf-theme=theme.yml -o output/book.pdf book.adoc
